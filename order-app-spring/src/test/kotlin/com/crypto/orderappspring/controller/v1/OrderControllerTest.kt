d7d2e2a84e005b43f985d3c9a143d03c7efb35e5
Dmitrii dmitriev <dmi3ev8@gmail.com>
2023-06-13 02:44:05 +0300

    OTUS-6: Fixed unit tests in spring

--- order-app-spring/src/test/kotlin/com/crypto/orderappspring/controller/v1/OrderControllerTest.kt
+++ order-app-spring/src/test/kotlin/com/crypto/orderappspring/controller/v1/OrderControllerTest.kt
@@ -2,13 +2,17 @@ package com.crypto.orderappspring.controller.v1
 
 import com.crypto.api.v1.models.*
 import com.crypto.common.CommonContext
+import com.crypto.logic.OrderProcessor
 import com.crypto.mappers.toTransportCreate
 import com.crypto.mappers.toTransportDelete
 import com.crypto.mappers.toTransportRead
 import com.crypto.mappers.toTransportSearch
+import com.crypto.orderappspring.service.OrderBlockingProcessor
 import com.crypto.stubs.OrderStub
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.ninjasquad.springmockk.MockkBean
 import io.kotest.assertions.print.print
+import io.mockk.coVerify
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -25,18 +29,21 @@ internal class OrderControllerTest {
     @Autowired
     private lateinit var mapper: ObjectMapper
 
+    @MockkBean(relaxUnitFun = true)
+    private lateinit var processor: OrderBlockingProcessor
+
     @Test
     fun createOrder() = testStubOrder(
         "/api/v1/order/create",
         OrderCreateRequest(),
-        CommonContext().apply { orderResponse = OrderStub.get() }.toTransportCreate()
+        CommonContext().toTransportCreate()
     )
 
     @Test
     fun readOrder() = testStubOrder(
         "/api/v1/order/read",
         OrderReadRequest(),
-        CommonContext().apply { orderResponse = OrderStub.get() }.toTransportRead()
+        CommonContext().toTransportRead()
     )
 
     @Test
@@ -50,7 +57,7 @@ internal class OrderControllerTest {
     fun searchOrder() = testStubOrder(
         "/api/v1/order/search",
         OrderSearchRequest(),
-        CommonContext().apply { ordersResponse.add(OrderStub.get()) }.toTransportSearch()
+        CommonContext().toTransportSearch()
     )
 
     private fun <Req : Any, Res : Any> testStubOrder(url: String, requestObj: Req, responseObj: Res) {
@@ -65,5 +72,6 @@ internal class OrderControllerTest {
             .andDo { print() }
             .andExpect(MockMvcResultMatchers.status().isOk)
             .andExpect(MockMvcResultMatchers.content().json(response))
+        coVerify { processor.exec(any()) }
     }
 }
